682d65afbf862dcb4f97d503d2ba2d6106befad3
Şeyma Bayrak <s.bayrak.89@hotmail.com>
2014-05-13 03:22:54 +0200

    change iteration range

--- lyapunov_expo.py
+++ lyapunov_expo.py
@@ -6,6 +6,11 @@ import random as rn
 import math as mt
 import pylab as pl
 
+import numpy as np
+import random as rn
+import math as mt
+import pylab as pl
+
 r_range = np.linspace(3,4,1000)
 Ly_Ex = np.zeros( len(r_range))
 
@@ -17,18 +22,18 @@ N = 2000
 
 n = 0
 
-for r in r_range : 
+for r in r_range :
 	x_init = rn.random()
 	summ = 0
-	
+
 	for i in range(0,N):
 		x_old = x_init
 		# Logistic Map
 		x_init = f(x_old,r)
-		
+
 		if i > N_pre:
 			summ = summ + g(x_init,r)
-			
+
 	summ = float(summ)/N
 	Ly_Ex[n] = summ
 	n = n+1
@@ -38,5 +43,3 @@ pl.ylabel('Lyapunov Exponent, $\lambda$', fontsize =20)
 pl.axis((r_range[0], r_range[-1],-1,1))
 pl.plot(r_range , Ly_Ex, 'r')
 pl.show()	
-
-
