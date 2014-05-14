ba0dbc1366b9709dd29f896a326a17209387e5bf
Şeyma Bayrak <s.bayrak.89@hotmail.com>
2014-05-14 10:51:58 +0200

    fix the r ranges

--- logistic_map.m
+++ logistic_map.m
@@ -1,6 +1,6 @@
 Npre = 5000; Nplot = 300;
 x = zeros(Nplot,1);
-for r = 2.5:0.0001:3.7,
+for r = 0:0.001:4,
   x(1) = 0.5;
   for n = 1:Npre,
     x(1) = r*x(1)*(1 - x(1));
@@ -12,25 +12,25 @@ for r = 2.5:0.0001:3.7,
   hold on;
 end,
 set(gca,'FontSize',25)
-title('Bifurcation diagram of the logistic map');
-xlabel('r');  ylabel('x_n');
-r1 = 2.998;
-r2 = 3.449;
-r3 = 3.544;
-r4 = 3.564;
-r5=3.569;
+%title('Bifurcation diagram of the logistic map');
+%xlabel('r');  ylabel('x_n');
+%r1 = 2.998;
+%r2 = 3.449;
+%r3 = 3.544;
+%r4 = 3.564;
+%r5=3.569;
 
-line([r1 r1],[0.2 1],'Color','r','LineWidth',0.2)
-line([r2 r2],[0.2 1],'Color','r','LineWidth',0.2)
-line([r3 r3],[0.2 1],'Color','r','LineWidth',0.2)
-line([r4 r4],[0.2 1],'Color','r','LineWidth',0.2)
-line([r5 r5],[0.2 1],'Color','r','LineWidth',0.2)
+%line([r1 r1],[0.2 1],'Color','r','LineWidth',0.2)
+%line([r2 r2],[0.2 1],'Color','r','LineWidth',0.2)
+%line([r3 r3],[0.2 1],'Color','r','LineWidth',0.2)
+%line([r4 r4],[0.2 1],'Color','r','LineWidth',0.2)
+%line([r5 r5],[0.2 1],'Color','r','LineWidth',0.2)
 set(gca, 'xlim');
-axis([2.5, 3.7, 0.2 , 1])
-hold off; 
+axis([0, 4, -1 , 1])
+%hold off; 
 
 r = 0:0.001:4;
-a = length(r);
+N = length(r);
 
 lyap=zeros(1,N);
 j=0;
@@ -38,7 +38,7 @@ for(r=0:0.001:4)
     xn1=rand(1);
     lyp=0;
     j=j+1;
-    for(i=1:10000)
+    for(i=1:2300)
         xn=xn1;
         %logistic map
         xn1=r*xn*(1-xn);
@@ -53,4 +53,8 @@ for(r=0:0.001:4)
     lyap(j)=lyp;
 end
 r = 0:0.001:4;
-plot(r,lyap);
+axis([0,4,-1,1])
+plot([0 4],[0 0],'Color','k','linestyle','--','LineWidth',1)
+plot(r,lyap,'r');
+xlabel('r');  ylabel('x_n and \lambda');
+hold off
--- lyapunov_expo.py
+++ lyapunov_expo.py
@@ -11,13 +11,13 @@ import random as rn
 import math as mt
 import pylab as pl
 
-r_range = np.linspace(3,4,1000)
+r_range = np.linspace(0.01,4,1000)
 Ly_Ex = np.zeros( len(r_range))
 
 f = lambda x,r : r*x*(1-x)
 g = lambda x,r : mt.log(abs(r-2*r*x))
 
-N_pre = 300
+N_pre = 400
 N = 2000
 
 n = 0
@@ -40,6 +40,6 @@ for r in r_range :
 
 pl.xlabel('r', fontsize = 20)
 pl.ylabel('Lyapunov Exponent, $\lambda$', fontsize =20)
-pl.axis((r_range[0], r_range[-1],-1,1))
+pl.axis((0, 4,-1,1))
 pl.plot(r_range , Ly_Ex, 'r')
 pl.show()	
